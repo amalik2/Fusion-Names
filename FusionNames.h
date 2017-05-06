9f82256be34a6305870cfab58c85d7902b1d7abd
Adil Malik <619mwa@gmail.com>
2017-05-05 21:08:50 -0600

    Update FusionNames.h

--- FusionNames.h
+++ FusionNames.h
@@ -4,7 +4,7 @@
 
 // Determine if the specified character is a letter or not.
 // If the uppercase version of the char is the same as the lowercase version, it is not a letter
-
 bool isCharNonLetter(char c);
+
 // Return a fusion name based on other and base
 std::string getFusionName(std::string base, std::string other);
